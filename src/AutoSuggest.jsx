379695de89357163b67a67638eccbd594d522af4
Denis Nasyrtdinov <work@denisnd.ru>
2018-04-17 13:14:35 +0700

    Keyboard handling

--- src/AutoSuggest.jsx
+++ src/AutoSuggest.jsx
@@ -3,6 +3,11 @@ import PropTypes from 'prop-types';
 
 import './AutoSuggest.scss';
 
+const KC_UP = 38;
+const KC_DOWN = 40;
+const KC_ENTER = 13;
+const KC_ESCAPE = 27;
+
 export default class AutoSuggest extends React.Component {
 	constructor(props) {
 		super(props);
@@ -11,6 +16,7 @@ export default class AutoSuggest extends React.Component {
 			value: '',
 			dropdownItems: [],
 			dropdownShown: false,
+			itemSelected: undefined,
 			cachedSearches: {}
 		};
 	}
@@ -45,18 +51,60 @@ export default class AutoSuggest extends React.Component {
 
 	}
 
-	onSelect(name) {
+	selectItem(name) {
 		this.setState({
 			value: name,
-			dropdownShown: false
 		});
+
+		this.hideDropdown();
+	}
+
+	hideDropdown() {
+		this.setState({
+			dropdownShown: false,
+			itemSelected: undefined
+		});
+	}
+
+	onKeyDown(keyCode) {
+		const {dropdownItems, dropdownShown, itemSelected} = this.state;
+
+		switch(keyCode) {
+			case KC_DOWN:
+			case KC_UP:
+				if (!dropdownItems.length) return;
+
+				let newItemSelected = typeof itemSelected != 'undefined' ? itemSelected : -1;
+
+				if (keyCode == KC_DOWN) {
+					if (newItemSelected+1 < dropdownItems.length) newItemSelected++;
+					else newItemSelected = 0;
+				} else if (keyCode == KC_UP) {
+					if (newItemSelected > 0) newItemSelected--;
+					else newItemSelected = dropdownItems.length-1;
+				}
+
+				this.setState({itemSelected: newItemSelected});
+				break;
+			case KC_ENTER:
+				if (dropdownShown && dropdownItems && dropdownItems[itemSelected]) {
+					this.selectItem(dropdownItems[itemSelected]);
+				}
+				break;
+			case KC_ESCAPE:
+				if (dropdownShown) {
+					this.hideDropdown();
+				}
+				break;
+		}
 	}
 
 	render() {
 		const {
 			value,
 			dropdownShown,
-			dropdownItems
+			dropdownItems,
+			itemSelected
 		} = this.state;
 
 		return (
@@ -65,13 +113,22 @@ export default class AutoSuggest extends React.Component {
 					type="text"
 					value={value}
 					onChange={(e) => this.onChange(e.target.value)}
-					placeholder="Search through our records"
+					onKeyDown={(e) => this.onKeyDown(e.keyCode)}
+					onBlur={() => this.hideDropdown()}
+					ref={(ref) => this.input = ref}
+					placeholder="Search fruits"
 					className="autosuggest__input"
+					autoComplete="off"
 				/>
 				{dropdownShown && 
 					<div className="autosuggest__dropdown">
 						{dropdownItems.map((item, index) =>
-							<AutoSuggestItem name={item} onSelect={(name) => this.onSelect(name)} key={index}/>
+							<AutoSuggestItem
+								name={item}
+								selectItem={(name) => this.selectItem(name)}
+								key={index}
+								selected={itemSelected === index}
+							/>
 						)}
 					</div>
 				}
@@ -93,10 +150,10 @@ const NOOP = function() {};
 
 class AutoSuggestItem extends React.Component {
 	render() {
-		const {name, onSelect = NOOP} = this.props;
+		const {name, selectItem = NOOP, selected} = this.props;
 
 		return (
-			<div className="autosuggest__item" onClick={() => onSelect(name)}>
+			<div className={'autosuggest__item' + (selected ? ' selected' : '')} onClick={() => selectItem(name)} ref={(ref) => this.ref = ref}>
 				{name}
 			</div>
 		)
